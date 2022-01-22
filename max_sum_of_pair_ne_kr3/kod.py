5a48ef20c0672b6b4877878bc8d1dc40ca5a6e51
dom <fenix15-05@mail.ru>
2022-01-23 01:46:33 +0300

    Выбрать одно число из каждой пары, найти максимальную сумму, не кратную трём

---
+++ max_sum_of_pair_ne_kr3/kod.py
@@ -0,0 +1,34 @@
+# Задание 27 № 27889
+# Имеется набор данных, состоящий из пар положительных целых чисел. 
+# Необходимо выбрать из каждой пары ровно одно число так, 
+# Чтобы сумма всех выбранных чисел не делилась на 3 и при этом была максимально возможной.
+
+
+from re import S
+
+
+file = open('27-A_demo.txt', 'r')
+b = int(file.readline())
+summa = 0
+minofpair = []
+
+for i in range(b):
+    a = file.readline()
+    a = a.split()
+    a[0] = int(a[0])
+    a[1] = int(a[1])
+    summa += int(max(a))
+    minofpair.append(int(abs(a[0]-a[1])))
+
+minofpair.sort()
+print(minofpair)
+
+for i in range(len(minofpair)):
+    if (summa % 3) != 0:
+        #print(summa)
+        break
+    elif ((summa-minofpair[i]) % 3 != 0):
+        #print(summa-minofpair[i])
+        break
+
+
